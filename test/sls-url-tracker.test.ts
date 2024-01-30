3eb671ef19b02e25ca143b69967a9faddc92fd9a
Luca Bianchi <bianchi.luca@gmail.com>
2024-01-30 21:58:54 +0100

    Initial commit

---
+++ test/sls-url-tracker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SlsUrlTracker from '../lib/sls-url-tracker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sls-url-tracker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SlsUrlTracker.SlsUrlTrackerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
